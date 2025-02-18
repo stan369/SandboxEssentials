7bb24acb843ced0e74f55671f95d29514d4beedc
Stan <redfishslayer2014@gmail.com>
2025-02-18 00:00:09 -0500

    Update blacklist.lua
    Switched Blacklist to Tables for faster checks. 
    Added a Weapon Blacklist into the Blacklist.lua

--- sandbox_essentials/lua/autorun/server/blacklist.lua
+++ sandbox_essentials/lua/autorun/server/blacklist.lua
@@ -16,42 +16,72 @@
 -- Stan https://steamcommunity.com/id/Stan369
 
 
--- Define a list of NPCs that players are not allowed to spawn
+-- Chose the table route for faster checks. 
 local disallowedNPCs = {
-    "npc_helicopter",
-    "npc_combinedropship",
-	    "npc_combinegunship",
-		    "npc_strider",
-			    "npc_tf2_ghost",
-    -- Add more NPCs here if needed
+    npc_helicopter = true,
+    npc_combinedropship = true,
+    npc_combinegunship = true,
+    npc_strider = true,
+    npc_tf2_ghost = true
+	-- Add more NPCs here if needed
 }
 
--- Define a list of entities that players are not allowed to spawn
 local disallowedEntities = {
-	"sent_ball",
---  "",
---	"",
---	"",
-    -- Add more entities here if needed remove the two - and add entity between ""
+    sent_ball = true,
+    npc_grenade_frag = true,
+    item_rpg_round = true,
+    item_ammo_smg1_grenade = true,
+    grenade_helicopter = true,
+    combine_mine = true
+	    -- Add more entities here if needed 
 }
+
+local disallowedWeapons = {
+    weapon_rpg = true,
+    weapon_frag = true,
+    weapon_slam = true,
+    manhack_welder = true,
+    bb_cssfrag_alt = true
+	    -- Add more weapons here if needed.
+}
+
+-- Below are the hooks to disallow. 
 
 -- Hook into player spawn functions
 hook.Add("PlayerSpawnNPC", "DisallowNPCSpawn", function(ply, npcType, weapon)
-    -- Check if the NPC being spawned is in the disallowed list
-    if table.HasValue(disallowedNPCs, npcType) then
-        -- Inform the player that they cannot spawn this NPC
-        ply:PrintMessage(HUD_PRINTTALK, "[SBoxEssentials] You are not allowed to spawn this NPC.")
-        -- Return false to prevent the NPC from spawning
+    if disallowedNPCs[npcType] then
+        ply:PrintMessage(HUD_PRINTTALK, "[TheSBoxServer] You are not allowed to spawn this NPC.") -- NPC Blacklist Message
         return false
     end
 end)
 
 hook.Add("PlayerSpawnSENT", "DisallowEntitySpawn", function(ply, class)
-    -- Check if the entity being spawned is in the disallowed list
-    if table.HasValue(disallowedEntities, class) then
-        -- Inform the player that they cannot spawn this entity
-        ply:PrintMessage(HUD_PRINTTALK, "[SBoxEssentials] You are not allowed to spawn this entity.")
-        -- Return false to prevent the entity from spawning
+    if disallowedEntities[class] then
+        ply:PrintMessage(HUD_PRINTTALK, "[TheSBoxServer] You are not allowed to spawn this entity.") -- Entity Blacklist Message
+        return false
+    end
+end)
+
+-- Prevent giving weapons via commands (give <weapon>)
+hook.Add("PlayerGiveSWEP", "DisallowWeaponSpawn", function(ply, weaponClass, swep)
+    if disallowedWeapons[weaponClass] then
+        ply:PrintMessage(HUD_PRINTTALK, "[TheSBoxServer] You are not allowed to spawn this weapon.") -- Weapon Blacklist Message
+        return false
+    end
+end)
+
+-- Prevent spawning weapons via Q menu
+hook.Add("PlayerSpawnSWEP", "DisallowSWEPSpawn", function(ply, weaponClass, swep)
+    if disallowedWeapons[weaponClass] then
+        ply:PrintMessage(HUD_PRINTTALK, "[TheSBoxServer] You are not allowed to spawn this weapon.") -- Weapon Blacklist Message
+        return false
+    end
+end)
+
+-- Prevent picking up blacklisted weapons
+hook.Add("PlayerCanPickupWeapon", "DisallowWeaponPickup", function(ply, weapon)
+    if disallowedWeapons[weapon:GetClass()] then
+        ply:PrintMessage(HUD_PRINTTALK, "[TheSBoxServer] You are not allowed to use this weapon.") -- Weapon Blacklist Message
         return false
     end
 end)
